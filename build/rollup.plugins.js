84c03811fcc8139a4bcb46528770d91835e2ebcf
y1j2x34 <y1j2x34@qq.com>
2021-10-12 21:54:22 +0800

    chore: commitlint config & terser plugin

--- build/rollup.plugins.js
+++ build/rollup.plugins.js
@@ -4,7 +4,7 @@ const nodeBuiltins = require('rollup-plugin-node-builtins');
 const commonjs = require('@rollup/plugin-commonjs');
 const strip = require('@rollup/plugin-strip');
 const json = require('@rollup/plugin-json');
-const uglify = require('rollup-plugin-uglify');
+const terser = require('rollup-plugin-terser');
 
 function ext(opt1, opt2) {
     if(opt2 && opt1) {
@@ -48,11 +48,9 @@ module.exports = {
     commonjs(opt){
         return commonjs(ext({
             include: ['node_modules/**'],
-            // namedExports: {
-            // }
         }, opt));
     },
-    uglify(opt) {
-        return uglify(ext({}, opt));
+    terser(opt) {
+        return terser(ext({}, opt));
     }
 };
